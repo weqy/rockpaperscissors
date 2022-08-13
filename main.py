43a5b448537d8b56d0632c9db80f1e6c1c418e4a
weq <109391494+weqy@users.noreply.github.com>
2022-08-13 17:38:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,181 +1,97 @@
 import tkinter as tk
+from tkinter import *
 import random
 import playsound
+# imports all GUI commands, randomizer, and mp3 player
 
 root = tk.Tk()
+# canvas
 
 canvas1 = tk.Canvas(root, width=400, height=300, relief='raised')
 canvas1.pack()
+# canvas dimensions
 
-label1 = tk.Label(root, text='Word Guessing Game')
+label1 = tk.Label(root, text='Rock, Paper, Scissors')
 label1.config(font=('helvetica', 14))
 canvas1.create_window(200, 25, window=label1)
+# title
 
-my_words = ["rainbow", "gold", "dog"]
-random_word = random.choice(my_words)
+label2 = tk.Label(root, text='Which would you like to play as?')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(200, 100, window=label2)
+# tells user what to enter in widget
 
 entry1 = tk.Entry(root)
+canvas1.create_window(200, 140, window=entry1)
+# entry widget
 
-def create_canvas():
-    canvas1.delete("all")
+choices = ["rock", "paper", "scissors"]
+my_choice = random.choice(choices)
+# random choice from computer
 
-    label1 = tk.Label(root, text='Word Guessing Game')
-    label1.config(font=('helvetica', 14))
-    canvas1.create_window(200, 25, window=label1)
 
-    canvas1.create_window(200, 140, window=entry1)
+def clear_text():
+   entry1.delete(0, END)
+# delete entry text
 
-    button1 = tk.Button(text='Enter', command=see_if_correct, bg='brown', fg='white',
-                        font=('helvetica', 9, 'bold'))
-    canvas1.create_window(200, 180, window=button1)
 
-    my_disclaimer = tk.Label(root, text='Type in all lowercase.')
-    my_disclaimer.config(font=('helvetica', 10))
-    canvas1.create_window(200, 200, window=my_disclaimer)
-
-    if random_word == "rainbow":
-        label2 = tk.Label(root, text='I am a seven-letter word. I am very colorful, \nbut I only show myself '
-                                     'in bad weather. \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "gold":
-        label2 = tk.Label(root, text='I am a four-letter word and I am a very expensive metal. \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "dog":
-        label2 = tk.Label(root, text="I am a three letter word that is man's best friend. \nWhat am I?")
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
+def user_entry_check():
+    if "scissors" in entry1.get():
+        rock_paper_scissors()
+    elif "rock" in entry1.get():
+        rock_paper_scissors()
+    elif "paper" in entry1.get():
+        rock_paper_scissors()
     else:
-        print("Something went wrong!")
+        error = tk.Label(root, fg="red", text="Please check your entry.")
+        error.config(font=('helvetica', 10))
+        canvas1.create_window(200, 220, window=error)
+        clear_text()
+        # checks user entry
 
 
-def see_if_correct():
-    guess_num = 3
-    if random_word in entry1.get():
-        canvas1.delete("all")
-        result = tk.Label(root, text="You are correct! Congratulations!\n\n\n :D")
+def rock_paper_scissors():
+    if my_choice == entry1.get():
+        result = tk.Label(root, text="Tie! Both players chose " + my_choice)
         result.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=result)
+        canvas1.create_window(200, 220, window=result)
+    elif my_choice == "scissors" and "rock" in entry1.get():
+        result = tk.Label(root, text="User wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
+        result.config(font=('helvetica', 10))
+        canvas1.create_window(200, 220, window=result)
         playsound.playsound('winner.mp3')
-    else:
-        guess_num -= 1
-        result = tk.Label(root, text="Try again. You have " + str(guess_num) + " guesses left.")
+    elif my_choice == "rock" and "scissors" in entry1.get():
+        result = tk.Label(root, text="Computer wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
         result.config(font=('helvetica', 10))
-        canvas1.create_window(200, 230, window=result)
-        create_canvas2()
+        canvas1.create_window(200, 220, window=result)
         playsound.playsound('loser.mp3')
-
-
-def create_canvas2():
-    canvas1.delete("all")
-
-    label1 = tk.Label(root, text='Word Guessing Game')
-    label1.config(font=('helvetica', 14))
-    canvas1.create_window(200, 25, window=label1)
-
-    canvas1.create_window(200, 140, window=entry1)
-
-    button1 = tk.Button(text='Enter', command=see_if_correct2, bg='brown', fg='white',
-                        font=('helvetica', 9, 'bold'))
-    canvas1.create_window(200, 180, window=button1)
-
-    result = tk.Label(root, text="Try again. You have 2 guesses left.")
-    result.config(font=('helvetica', 10))
-    canvas1.create_window(200, 230, window=result)
-
-    my_disclaimer = tk.Label(root, text='Type in all lowercase.')
-    my_disclaimer.config(font=('helvetica', 10))
-    canvas1.create_window(200, 200, window=my_disclaimer)
-
-    if random_word == "rainbow":
-        label2 = tk.Label(root, text='I am often seen when the rain and sunlight collide. \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "gold":
-        label2 = tk.Label(root, text='I am a very heavy metal that is worth a lot of money. \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "dog":
-        label2 = tk.Label(root, text="When I want to communicate, I bark. \nWhat am I?")
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    else:
-        print("Something went wrong!")
-
-def see_if_correct2():
-    guess_num = 2
-    if random_word in entry1.get():
-        canvas1.delete("all")
-        result = tk.Label(root, text="You are correct! Congratulations!\n\n\n :D")
+    elif my_choice == "paper" and "rock" in entry1.get():
+        result = tk.Label(root, text="Computer wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
         result.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=result)
+        canvas1.create_window(200, 220, window=result)
+        playsound.playsound('loser.mp3')
+    elif my_choice == "rock" and "paper" in entry1.get():
+        result = tk.Label(root, text="User wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
+        result.config(font=('helvetica', 10))
+        canvas1.create_window(200, 220, window=result)
         playsound.playsound('winner.mp3')
-    else:
-        guess_num -= 1
-        create_canvas3()
-        playsound.playsound('loser.mp3')
-
-
-def create_canvas3():
-    canvas1.delete("all")
-
-    label1 = tk.Label(root, text='Word Guessing Game')
-    label1.config(font=('helvetica', 14))
-    canvas1.create_window(200, 25, window=label1)
-
-    canvas1.create_window(200, 140, window=entry1)
-
-    button1 = tk.Button(text='Enter', command=see_if_correct3, bg='brown', fg='white',
-                        font=('helvetica', 9, 'bold'))
-    canvas1.create_window(200, 180, window=button1)
-
-    my_disclaimer = tk.Label(root, text='Type in all lowercase.')
-    my_disclaimer.config(font=('helvetica', 10))
-    canvas1.create_window(200, 200, window=my_disclaimer)
-
-    result = tk.Label(root, text="Try again. You have 1 guess left.")
-    result.config(font=('helvetica', 10))
-    canvas1.create_window(200, 230, window=result)
-
-    if random_word == "rainbow":
-        label2 = tk.Label(root, text='I start with "R" and end with "W". \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "gold":
-        label2 = tk.Label(root, text='I start with "G" and end with "D". \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    elif random_word == "dog":
-        label2 = tk.Label(root, text='I start with "D" and end with "G". \nWhat am I?')
-        label2.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=label2)
-    else:
-        print("Something went wrong!")
-
-
-def see_if_correct3():
-    guess_num = 1
-    if random_word in entry1.get():
-        canvas1.delete("all")
-        result = tk.Label(root, text="You are correct! Congratulations!\n\n\n :D")
+    elif my_choice == "paper" and "scissors" in entry1.get():
+        result = tk.Label(root, text="User wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
         result.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=result)
+        canvas1.create_window(200, 220, window=result)
         playsound.playsound('winner.mp3')
-    else:
-        canvas1.delete("all")
-        result = tk.Label(root, text="You have no more guesses left. Try again!\n\n\n :(")
+    elif my_choice == "scissors" and "paper" in entry1.get():
+        result = tk.Label(root, text="Computer wins! User chose " + entry1.get() + "\n and computer chose " + my_choice)
         result.config(font=('helvetica', 10))
-        canvas1.create_window(200, 100, window=result)
         playsound.playsound('loser.mp3')
+        # all possible outcomes of rock paper scissors
 
 
 
-button1 = tk.Button(text='Generate Random Word', command=create_canvas, bg='brown', fg='white',
+button1 = tk.Button(text='Play!', command=user_entry_check, bg='brown', fg='white',
                     font=('helvetica', 9, 'bold'))
-canvas1.create_window(200, 150, window=button1)
-
-
-
+canvas1.create_window(200, 180, window=button1)
+# button to start process
 
 root.mainloop()
+# shows window
